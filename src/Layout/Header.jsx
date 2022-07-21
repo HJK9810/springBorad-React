7fb5b300dfe6e27294a5e70fc0797235a647ae53
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-07-21 21:17:15 +0900

    brand click go root

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header>
       <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
